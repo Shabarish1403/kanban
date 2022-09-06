2ebc0b38a2e06d704485fb6d848d56dfbcca6677
shabarish <shabarish.14b@gmail.com>
2022-09-06 15:28:10 +0530

    relationships upgrade

--- application/controllers.py
+++ application/controllers.py
@@ -24,18 +24,21 @@ def login():
     
 @app.route("/home/<user_name>", methods=["GET","POST"])
 def home(user_name):
-    lists = List.query.filter_by(user_name=user_name).all()
+    user = User.query.filter_by(user_name=user_name).first()
+    lists = user.lists
+    #lists = List.query.filter_by(user_name=user_name).all()
 
     cards = {}
     for list in lists:
-        print(list.cards)
-        cards_list = Card.query.filter_by(list_id=list.list_id).all()
-        cards[list.list_name] = cards_list
+        #print(list.cards)
+        #cards_list = Card.query.filter_by(list_id=list.list_id).all()
+        cards[list.list_name] = list.cards
 
     return render_template("home.html",user_name=user_name,lists=lists,cards=cards)
 
 @app.route('/<user_name>/createlist',methods=["GET","POST"])
 def createlist(user_name):
+    user = User.query.filter_by(user_name=user_name).first()
     if request.method=="GET":
         return render_template("create_list.html",user_name=user_name)
     elif request.method=="POST":
@@ -43,9 +46,9 @@ def createlist(user_name):
         description = request.form['description']
 
         #need to add an alert in browser if listname already exists
-        list = List.query.filter_by(user_name=user_name,list_name=list_name).first()
+        list = List.query.filter_by(list_name=list_name,user=user).first()
         if list == None:
-            l = List(user_name=user_name,list_name=list_name,description=description)
+            l = List(list_name=list_name,description=description,user=user)
             db.session.add(l)
             db.session.commit()
 
@@ -53,7 +56,8 @@ def createlist(user_name):
 
 @app.route('/<user_name>/<list_name>/deletelist',methods=["GET","POST"])
 def deletelist(user_name,list_name):
-    del_list = List.query.filter_by(user_name=user_name,list_name=list_name).first()
+    user = User.query.filter_by(user_name=user_name).first()
+    del_list = List.query.filter_by(list_name=list_name,user=user).first()
     db.session.delete(del_list)
     db.session.commit()
     return redirect(url_for('home',user_name=user_name))
@@ -61,11 +65,12 @@ def deletelist(user_name,list_name):
 @app.route('/<user_name>/<list_name>/updatelist',methods=["GET","POST"])
 def updatelist(user_name,list_name):
     #need to add go back or home button
-    list = List.query.filter_by(user_name=user_name,list_name=list_name).first()
+    user = User.query.filter_by(user_name=user_name).first()
+    list = List.query.filter_by(list_name=list_name,user=user).first()
     if request.method == "GET":
         return render_template('update_list.html',user_name=user_name,list=list)
     elif request.method == "POST":
-        l = List.query.filter_by(user_name=user_name,list_name=request.form['name']).first()
+        l = List.query.filter_by(list_name=request.form['name'],user=user).first()
 
         if list.list_name == request.form['name']:
             list.description = request.form['description']
@@ -78,6 +83,7 @@ def updatelist(user_name,list_name):
 
 @app.route('/<user_name>/<list_name>/createcard',methods=['POST','GET'])
 def createcard(user_name,list_name):
+    user = User.query.filter_by(user_name=user_name).first()
     if request.method == "GET":
         return render_template('create_card.html',user_name=user_name,list_name=list_name)
     elif request.method == "POST":
@@ -85,19 +91,20 @@ def createcard(user_name,list_name):
         content = request.form['content']
         deadline = request.form['deadline']
 
-        list = List.query.filter_by(user_name=user_name, list_name=list_name).first()
-        card = Card.query.filter_by(list_id = list.list_id, card_name=card_name).first()
+        list = List.query.filter_by(list_name=list_name,user=user).first()
+        card = Card.query.filter_by(card_name=card_name,list = list).first()
 
         if card == None:
-            c = Card(list_id=list.list_id,card_name=card_name,content=content,deadline=deadline)
+            c = Card(card_name=card_name,content=content,deadline=deadline,list=list)
             db.session.add(c)
             db.session.commit()
         return redirect(url_for("home",user_name=user_name))
 
 @app.route('/<user_name>/<list_name>/<card_name>/deletecard',methods=["GET","POST"])
 def deletecard(user_name,list_name,card_name):
-    list = List.query.filter_by(user_name=user_name,list_name=list_name).first()
-    del_card = Card.query.filter_by(list_id=list.list_id,card_name=card_name).first()
+    user = User.query.filter_by(user_name=user_name).first()
+    list = List.query.filter_by(list_name=list_name,user=user).first()
+    del_card = Card.query.filter_by(card_name=card_name,list=list).first()
     db.session.delete(del_card)
     db.session.commit()
     return redirect(url_for('home',user_name=user_name))
@@ -105,14 +112,15 @@ def deletecard(user_name,list_name,card_name):
 @app.route('/<user_name>/<list_name>/<card_name>/updatecard', methods=['POST','GET'])
 def updatecard(user_name,list_name,card_name):
     #need to add go back or home button
-    lists = List.query.filter_by(user_name=user_name).all()
-    list = List.query.filter_by(user_name=user_name,list_name=list_name).first()
-    card = Card.query.filter_by(list_id=list.list_id,card_name=card_name).first()
+    user = User.query.filter_by(user_name=user_name).first()
+    lists = user.lists
+    list = List.query.filter_by(list_name=list_name,user=user).first()
+    card = Card.query.filter_by(card_name=card_name,list=list).first()
     if request.method == "GET":
         return render_template('update_card.html',user_name=user_name,card=card,lists=lists,list=list)
     elif request.method == "POST":
         if list.list_name == request.form['list']: #need to alert if same card name exists in same list
-            c = Card.query.filter_by(list_id=list.list_id,card_name=request.form['name']).first()
+            c = Card.query.filter_by(card_name=request.form['name'],list=list).first()
             if card.card_name == request.form['name']:
                 card.content = request.form['content']
                 card.deadline = request.form['deadline']
@@ -123,10 +131,10 @@ def updatecard(user_name,list_name,card_name):
                 card.deadline = request.form['deadline']
                 db.session.commit()
         else:
-            newlist = List.query.filter_by(user_name=user_name,list_name=request.form['list']).first()
-            c = Card.query.filter_by(list_id=newlist.list_id, card_name=request.form['name']).first()            
+            newlist = List.query.filter_by(list_name=request.form['list'],user=user).first()
+            c = Card.query.filter_by(card_name=request.form['name'],list=newlist).first()            
             if c == None:
-                card.list_id = newlist.list_id
+                card.list_id = newlist.id
                 card.card_name = request.form['name']
                 card.content = request.form['content']
                 card.deadline = request.form['deadline']
--- application/models.py
+++ application/models.py
@@ -3,22 +3,22 @@ from .database import db
 
 class User(db.Model):
     __tablename__ = 'user'
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_name = db.Column(db.String, unique=True)
-    #lists = db.relationship('List', secondary='user_list')
+    lists = db.relationship('List', backref='user')
     
 class List(db.Model):
     __tablename__ = 'list'
-    list_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_name = db.Column(db.String, db.ForeignKey('user.user_name'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     list_name = db.Column(db.String, nullable=False)
     description = db.Column(db.String)
+    user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable=False)
     cards = db.relationship('Card',backref='list',cascade='all,delete-orphan')
 
 class Card(db.Model):
     __tablename__ = 'card'
-    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     card_name = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
     deadline = db.Column(db.String, nullable=False)
-    list_id = db.Column(db.Integer, db.ForeignKey('list.list_id'), nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('list.id'), nullable=False)
